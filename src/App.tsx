36ddd7c575986dfa011be7f9741e576dd320acbb
RaddishAI <petter.arbeid@gmail.com>
2026-05-18 21:47:29 +0200

    feat: setup routing (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <MainLayout>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </MainLayout>
   );
 }
